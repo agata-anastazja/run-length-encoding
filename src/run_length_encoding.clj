60c7e2f522a132a09fa890d5a1d146d88a24bf1b
Agata <agata.sumowska@mypulse.ai>
2020-09-03 21:21:06 +0200

    refactor

--- src/run_length_encoding.clj
+++ src/run_length_encoding.clj
@@ -2,41 +2,31 @@
   (:require [clojure.string :as str])
   )
 
-(defn transform [elem]
-  (let [occurences (count elem)
-        firstElem (first elem)
-        back-to-str (fn [x] (apply str x))]
-    (if (> occurences 1)
-      (str occurences firstElem)
-      (back-to-str elem))))
-
-
 (defn  run-length-encode
   "decodes a run-length-encoded string"
   [plain-text]
-  (apply str (map transform (partition-by identity plain-text))))
+  (->>
+    (partition-by identity plain-text)
+    (mapcat (juxt count first))
+    (remove #(= %1 \1))
+    (apply str)))
 
 
-(defn find-first-integer [cipher]
-  (Integer/parseInt (re-find #"\d+" cipher) ) )
-
-(defn split-at-first-int [cipher]
-  (clojure.string/split cipher #"\d+" 2))
-
-(defn multiply-first-char [char int]
-  (apply str (take int (repeat char))))
 
 
 (defn run-length-decode
   "encodes a string with run-length-encoding"
   [cipher]
-    (let [[head tail] (split-at-first-int cipher)]
-      (if (= tail nil)
-        cipher
-        (let [first-int (find-first-integer cipher)
-              multiplied-char (first tail)
-              remaining (apply str (rest tail))]
-          (apply str head (multiply-first-char multiplied-char first-int) (run-length-decode remaining))))))
-
+  (->>
+    cipher
+    (re-seq #"(\d+)?(\D)")
+    (mapcat (fn [x]
+              (let [repetitions (nth x 1)
+                    char (nth x 2)]
+                (if repetitions
+                  (repeat (Integer/parseInt repetitions) char )
+                  char)
+                   )))
+    (apply str)))
 
 
